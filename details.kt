e303be958677e5f2b59a85f66cdf537bdc6eadef
jayson7 <lexxiijoo70@gmail.com>
2025-04-25 13:56:41 +0100

    mobile app frestyle

---
+++ details.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name = "Adebayo"  // Immutable
+    var age = 25           // Mutable
+    
+    println("Name: $name")
+    println("Age: $age")
+    
+    age = 26  // You can change mutable variables
+    println("New Age: $age")
+}
